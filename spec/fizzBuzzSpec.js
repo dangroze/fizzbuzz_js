f53e80eaeccfc8646a7ec7c88c46f2368aff7897
Dan Groze <dangroze@gmail.com>
2019-02-05 12:10:08 +0000

    Added tests for divisible by 5 and 15

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -13,7 +13,13 @@ describe('Fizzbuzz', function() {
     it('is not divisible by 3', function() {
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
     });
-    
-  });
+    it('is divisible by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+    it('is divisible by 15', function() {
+      expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
 
+  });
+  
 });
